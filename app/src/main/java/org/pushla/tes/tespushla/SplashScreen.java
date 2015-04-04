f231f37615935847562624f6c0c4a4f0ff797cd0
Rafi Putra Arriyan <ayamkip@gmail.com>
2015-04-04 12:25:54 +0700

    ubah warna sign in

--- app/src/main/java/org/pushla/tes/tespushla/SplashScreen.java
+++ app/src/main/java/org/pushla/tes/tespushla/SplashScreen.java
@@ -222,7 +222,7 @@ public class SplashScreen extends ActionBarActivity {
                 SplashScreen.this.finish();
                 SplashScreen.this.startActivity(mainIntent);
             }else {
-                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent mainIntent = new Intent(SplashScreen.this, SignIn.class);
 //                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
                 System.out.println("Banyaknya proyek = " + SplashScreen.listProyek.size());
                 SplashScreen.this.finish();
